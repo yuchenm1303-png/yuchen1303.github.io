1c2ed199a01c0581563f136534db873097ca0edf
yuchenm1303-png <yuchenm1303@gmail.com>
2026-05-10 13:09:20 +0800

    Add service worker for ledger app

---
+++ ai-ledger/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'ai-ledger-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest',
+  './icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+});
