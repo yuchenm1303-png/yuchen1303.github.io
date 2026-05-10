f60da76cd4d7f3a2c2e272b34b3c0addc274f65a
yuchenm1303-png <yuchenm1303@gmail.com>
2026-05-10 11:11:08 +0800

    Add Android PDF viewer app build file

---
+++ android-pdf-viewer/app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.yuchen.pdfviewer"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.yuchen.pdfviewer"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
